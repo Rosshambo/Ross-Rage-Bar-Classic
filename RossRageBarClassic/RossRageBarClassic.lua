693185c5979636b4374d7ebb3a6c53ba9b53a305
Rosshambo <29986937+Rosshambo@users.noreply.github.com>
2019-09-02 23:39:19 -0500

    Initial Upload

---
+++ RossRageBarClassic/RossRageBarClassic.lua
@@ -0,0 +1,59 @@
+equipmentSet = "";
+t = {[0] = "Mana", [1] = "Rage", [2] = "Focus", [3] = "Energy", [4] = "Happiness", [5] = "Runes", [6] = "Runic Power", [7] = "Soul Shards", [8] = "Eclipse", [9] = "Holy Power"};
+function RageBar_OnLoad(self)
+	self:RegisterEvent("UNIT_POWER_UPDATE");
+	self:RegisterEvent("PLAYER_LOGIN");
+	createBar();
+end
+
+--Handles the event fired whenever the players "power" changes.
+function RageBar_OnEvent(self, event, ...)
+	if (event == "UNIT_POWER_UPDATE") then
+		--createBar();
+		updateBar();
+	elseif (event == "PLAYER_LOGIN") then
+		--createBar();
+		updateBar();
+	end
+end
+function createBar()
+	power = UnitPower("player");
+	--Only shows percentage if you are using mana, otherwise removes the % and shrinks the window slightly.
+	if (UnitPowerType("player") == 0) then
+		r = string.len(power);
+		RossRageBarFrame:SetSize((r*7)+140, 43);
+		RossRageBarFrameText:SetText(t[UnitPowerType("player")]..": "..power.."\n".." ("..floor((power/UnitPowerMax("player"))*100).."%)");
+	--For Runic Power
+	elseif (UnitPowerType("player") == 6) then
+		RossRageBarFrame:SetSize(110, 27);
+		RossRageBarFrameText:SetText(t[UnitPowerType("player")]..": "..power);
+	--For rage and energy
+	else
+		RossRageBarFrame:SetSize(90, 27);
+		RossRageBarFrameText:SetText(t[UnitPowerType("player")]..": "..power);
+	end
+end
+
+function updateBar()
+	power = UnitPower("player");
+	--Only shows percentage if you are using mana, otherwise removes the % and shrinks the window slightly.
+	if (UnitPowerType("player") == 0) then
+		r = string.len(power);
+		--RossRageBarFrame:SetSize((r*7)+90, 27);
+		RossRageBarFrameText:SetText(t[UnitPowerType("player")]..": "..power.." ("..floor((power/UnitPowerMax("player"))*100).."%)");
+	--For Runic Power
+	elseif (UnitPowerType("player") == 6) then
+		--RossRageBarFrame:SetSize(110, 27);
+		RossRageBarFrameText:SetText(t[UnitPowerType("player")]..": "..power);
+	--For rage and energy
+	else
+		--RossRageBarFrame:SetSize(90, 27);
+		RossRageBarFrameText:SetText(t[UnitPowerType("player")]..": "..power);
+	end
+end
+
+--Code to implement a tooltip.
+function RageBar_ShowTooltip(self)
+	GameTooltip:SetText("Shift+Click to move\nCtrl+Click to resize");
+	GameTooltip:Show();
+end
